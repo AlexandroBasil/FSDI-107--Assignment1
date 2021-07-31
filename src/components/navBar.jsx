0d842007a2765db72a5febfb79ca4f83a72811c8
AlexandroBasil <85268308+AlexandroBasil@users.noreply.github.com>
2021-07-31 11:41:02 -0700

    Add files via upload

---
+++ src/components/navBar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';  //imrc
+import './navBar.css';
+
+class NavBar extends Component { //cc
+    state = {};
+    render() {
+        return ( <div className="navbar">NavBar will be here!</div> );
+    }
+
+    test() {
+        console.log("This is a test")
+    }
+
+}
+
+export default NavBar;
